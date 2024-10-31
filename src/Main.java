701222a559325ef10e00157bfdeaf85de8ebcf3d
BigSisterDora <ela.strigun.09@inbox.ru>
2024-10-31 12:05:55 +0200

    Добавил расчет дискриминанта

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Введите коэффициент a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Введите коэффициент b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Введите коэффициент c: ");
+        double c = scanner.nextDouble();
+
+        // Вычисляем дискриминант
+        double D = b * b - 4 * a * c;
+        System.out.println("Дискриминант D = " + D);
+
+        if (D > 0) {
+            // Два действительных корня
+            double x1 = (-b + Math.sqrt(D)) / (2 * a);
+            double x2 = (-b - Math.sqrt(D)) / (2 * a);
+            System.out.println("Корни уравнения:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        } else if (D == 0) {
+            // Один действительный корень
+            double x = -b / (2 * a);
+            System.out.println("Уравнение имеет один корень:");
+            System.out.println("x = " + x);
+        } else {
+            // Нет действительных корней
+            System.out.println("Уравнение не имеет действительных корней.");
         }
+
+        scanner.close();
     }
 }
